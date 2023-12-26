e72805eaf8943a18040e7a1c1e0aada63c3b42dc
Dimasta <110729046+sudo213@users.noreply.github.com>
2023-12-26 18:49:18 +0300

    Update deployer.py

--- deployer.py
+++ deployer.py
@@ -4,30 +4,28 @@ from starknet_py.contract import Contract
 from starknet_py.net.networks import TESTNET
 from utils import str_to_felt
 
+# Constants
 ERC20_FILE = ['contracts/MyToken.cairo']
-
 ERC20_NAME = str_to_felt("Pharaoh")
 ERC20_SYMBOL = str_to_felt("PRH")
 DECIMALS = 18
 INITIAL_SUPPLY = 210000000000000000
-
 OWNER = 0x03E6eB63F21EaA80bEF27B1B5F7f93e2bD5fA59712D6566E701e5a6e8D122838
 ALLOWED_AMOUNT = 1000
-TIMEDELTA = 86400 #24h
+TIMEDELTA = 86400  # 24h
 
-async def deploy():
+async def deploy_erc20_contract():
     client = GatewayClient(TESTNET)
     print("Deploying ERC20 Contract...")
-    erc20_contract = await Contract.deploy(
-        client=client,
-        compilation_source=ERC20_FILE,
-        constructor_args=[ERC20_NAME, ERC20_SYMBOL, DECIMALS, INITIAL_SUPPLY, OWNER]
-    )
+    
+    constructor_args = [ERC20_NAME, ERC20_SYMBOL, DECIMALS, INITIAL_SUPPLY, OWNER]
+    erc20_contract = await Contract.deploy(client=client, compilation_source=ERC20_FILE, constructor_args=constructor_args)
+    
     print(f'✨ ERC20 Contract deployed at {hex(erc20_contract.deployed_contract.address)}')
-
     await erc20_contract.wait_for_acceptance()
-    return (erc20_contract)
+    
+    return erc20_contract
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(deploy())
+    loop.run_until_complete(deploy_erc20_contract())
